b22eb56484d7830dbd14dff6b79c21bb871c7d02
Joelmir Moura <joelmir_moura@hotmail.com>
2024-07-01 07:56:50 -0300

    feat: adciona link para criar um produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -3,20 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { ProductsService } from '../../shared/services/product.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { MatCardModule} from  '@angular/material/card';
-import { MatButtonModule} from  '@angular/material/button';
+import { MatButton, MatButtonModule} from  '@angular/material/button';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent,RouterLink,MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
   products: Product[] = [];
   productsService = inject(ProductsService);
+routeParam: any;
   ngOnInit(){
     this.productsService.getAll().subscribe((products) => {
       this.products = products
